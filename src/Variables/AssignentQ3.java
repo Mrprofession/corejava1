6fbecb82b6c2f92b792c116c77017dbee6d5946a
Mrprofession <sidmohite9005@gmail.com>
2026-02-09 12:35:29 +0530

    Assignment 1

---
+++ src/Variables/AssignentQ3.java
@@ -0,0 +1,12 @@
+package Variables;
+
+public class AssignentQ3 {
+
+	public static void main(String[] args) {
+		String a = "Hello";
+		String b = "World";
+			System.out.println(a+" "+b);
+
+	}
+
+}
