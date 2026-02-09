7d78221a728fcecb93b94eadf274e9af46686198
Mrprofession <sidmohite9005@gmail.com>
2026-02-09 12:15:17 +0530

    Initial commit

---
+++ src/Variables/DifferentDatatypes.java
@@ -0,0 +1,18 @@
+	package Variables;
+
+public class DifferentDatatypes {
+
+	public static void main(String[] args) {
+		int a = 10;
+		double b = 10.10;
+		char c ='H';
+		boolean flag = true;
+		String name = "Siddhesh";
+				System.out.println(a);
+		        System.out.println(b);
+		        System.out.println(c);
+		        System.out.println(flag);
+		        System.out.println(name);
+	}
+
+}
